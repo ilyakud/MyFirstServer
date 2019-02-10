478d38358b492542f50626f72d51a037c58ca99d
ilyakud <ilya9.96@mail.ru>
2019-02-10 12:04:05 +0300

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -5,13 +5,13 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+/*$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
-});
+});*/
 
 $app->get('/firstRequest', function (Request $request, Response $response, array $args) {
 	$params = $request->getQueryParams();
