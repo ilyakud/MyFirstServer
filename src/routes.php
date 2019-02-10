71cb64f92ca85b7421cf721291ba658f18be4c4e
ilyakud <ilya9.96@mail.ru>
2019-02-10 12:01:07 +0300

    Update routes.php
    Create my first route

--- src/routes.php
+++ src/routes.php
@@ -12,3 +12,19 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->get('/firstRequest', function (Request $request, Response $response, array $args) {
+	$params = $request->getQueryParams();
+	if ($params["login"] == "admin" && $params["password"] == "admin") 
+	{
+		$result = array('message' => 'OK');
+		$code = 200;
+	} 
+	else 
+	{
+		$result = array('error' => 'Invalid user');
+		$code = 400;
+	}
+
+	return $response->withJson($result, $code);
+});
