9077c5ee799c6d7be9fefe4f47f3e1e883d10258
ilyakud <ilya9.96@mail.ru>
2019-02-11 21:33:18 +0300

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -28,3 +28,31 @@ $app->get('/firstRequest', function (Request $request, Response $response, array
 
 	return $response->withJson($result, $code);
 });
+
+// 1) admin admin 2) user1 123456 3) user2 123456
+$app->get('/friends', function (Request $request, Response $response, array $args) {
+	$params = $request->getQueryParams();
+	if ($params["login"] == "admin" && $params["password"] == "admin")
+	{
+		$result = array('message' => '200 friends');
+		$code = 200;
+	}
+	else if ($params["login"] == "user1" && $params["password"] == "123456")
+	{
+		$result = array('message' => '100 friends');
+		$code = 200;
+	}
+	else if ($params['login'] == 'user2' && $params['password'] == '123456')
+	{
+		$result = array('message' => '300 friends');
+		$code = 200;
+	}
+	else
+	{
+		$result = array('message' => 'Invalid user');
+		$code = 400;
+	}
+	return $response->withJson($result, $code);
+
+});
+?>
